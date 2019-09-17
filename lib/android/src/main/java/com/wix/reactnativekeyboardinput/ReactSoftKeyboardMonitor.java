d8c2263aec57ad1aa147b63a9ca969437ee29f1d
Bui Quoc Hung <qhu91it@gmail.com>
2019-09-17 14:35:58 +0700

    upgrade to RN 0.60

--- lib/android/src/main/java/com/wix/reactnativekeyboardinput/ReactSoftKeyboardMonitor.java
+++ lib/android/src/main/java/com/wix/reactnativekeyboardinput/ReactSoftKeyboardMonitor.java
@@ -1,7 +1,7 @@
 package com.wix.reactnativekeyboardinput;
 
 import android.graphics.Rect;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.view.ViewTreeObserver;
 
 import com.facebook.react.ReactRootView;
--- lib/android/src/main/java/com/wix/reactnativekeyboardinput/utils/ViewUtils.java
+++ lib/android/src/main/java/com/wix/reactnativekeyboardinput/utils/ViewUtils.java
@@ -1,7 +1,7 @@
 package com.wix.reactnativekeyboardinput.utils;
 
 import android.app.Activity;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
